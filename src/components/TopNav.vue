a95be3bdd5ae1842571277e9abcbe3da314f0808
HeartBeat <heartbeatpkmn@gmail.com>
2022-03-29 23:21:07 +0700

    click icon go to login

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class="px-3 py-4 flex items-center justify-between w-full">
     <div class="flex items-center">
-      <button class="rounded-full hover:bg-light p-3 flex items-center justify-center" type="button" name="button">
+      <button class="rounded-full hover:bg-light p-3 flex items-center justify-center" type="button" name="button" @click = "this.$router.push('/login')">
         <img src="logo.png" alt="logo" class="h-6 w-6">
       </button>
       <button v-for="(item, i) in tabItems" :key="i" @click="selectedTab = item.key" :class="`${selectedTab === item.key ? 'bg-dark text-white' : 'text-dark hover:bg-light'} rounded-full py-3 px-4 font-semibold`" type="button" name="button">
@@ -12,7 +12,7 @@
       <span class="material-icons absolute mt-3 ml-3 text-muted">search</span>
       <input class="text-muted w-full hover:bg-lightHover rounded-full bg-light py-3 pr-3 pl-10" placeholder="Search" type="text" name="" value="">
     </div>
-    <div class="flex items-center">
+    <div class="flex items-center"></div>
       <button type="button" name="button" class="relative rounded-full hover:bg-light flex items-center justify-center p-3">
         <span class="material-icons text-3xl text-muted">notifications</span>
         <div class="rounded-full h-5 w-5 bg-primary text-white flex items-center justify-center absolute mb-5 ml-5">4</div>
