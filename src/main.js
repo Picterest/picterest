9083214fb1cf71cd0609e279ae777db05c078dc2
Un <unn141042@gmail.com>
2022-03-30 00:50:02 +0700

    [ADD] Upload View
    the image can be display but not upload

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { IonicVue } from '@ionic/vue';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -24,4 +25,9 @@ const firebaseConfig = {
 // Initialize Firebase
 initializeApp(firebaseConfig);
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(IonicVue).use(router).use(store);
+
+router.isReady().then(() => {
+  app.mount('#app');
+});
+
---
+++ src/views/UploadView.vue
@@ -0,0 +1,61 @@
+<template>
+
+    <div class="flex justify-center">
+        <div class="mb-3 w-96">
+            <label for="formFile" class="form-label inline-block mb-2 text-gray-700">Default file input example</label>
+            <input class="form-control
+            block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFile"
+            @change="onFilePicked">
+        </div>
+    </div>
+
+    <div class="flex flex-wrap justify-center">
+        <img v-bind:src="imageUrl"
+        class="p-1 bg-white border rounded max-w-sm"
+        />
+        
+    </div>
+
+
+
+
+</template>
+
+<script>
+export default {
+    data() {
+       return {
+            imageUrl : ''
+            }
+    },
+
+    methods:{
+        onFilePicked(event) {
+            const files = event.target.files;
+            const fileReader = new FileReader();
+            fileReader.addEventListener('load',() => {
+                this.imageUrl = fileReader.result;
+            })
+            fileReader.readAsDataURL(files[0]);
+
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
