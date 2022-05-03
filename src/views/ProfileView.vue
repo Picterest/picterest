0029946592acd2c5da6286cd1f6425209f257a71
unn <62011286@kmitl.ac.th>
2022-05-03 20:16:23 +0700

    [UPDATE] Now data fetch from google  firestore.

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -8,16 +8,32 @@
         <div>{{UserName}}</div>
         <button class="bg-secondary text-white py-2 px-4 rounded-full">Edit Profile</button>
     </div>
+
+    <div v-for="(card, i) in arr" :key="i" class="w-full">
+      <div class="w-full p-2">
+        <Card :src="`${card.src}`" :board="`${card.recommended}`" class="w-full h-full"/>
+          <div class="text-sm leading-tight pt-2">
+            <p class="font-bold"> {{ card.title }} </p>
+            <p> {{ card.user }} </p>
+          </div>
+      </div>
+    </div>    
+
     <!-- For filter only user picture -->
     <div class="px-20 w-full flex flex-wrap"></div>
   </div>
+
 </template>
 
 <script>
 import TopNav from '../components/TopNav.vue';
-import { getAuth } from '@firebase/auth';
+import Card from '../components/CardItem.vue';
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import {db} from '@/firebase'
+
 export default {
-    components:{ TopNav },
+    components:{ TopNav,Card },
     data(){
         return{
             DisplayName: "",//"Firstname Lastname",
@@ -25,6 +41,28 @@ export default {
             cards:[],
         }
   },
+
+  created() {
+    const q = query(collection(db, "posts"),where("userId","==", getAuth().currentUser.uid));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    const cards = [];
+    querySnapshot.forEach((doc) => {
+      const post = doc.data()
+      let entry = {                       //Mapping to the correct format
+        src : post.imageDownloadUrl,
+        title: post.imageName,
+        user: "User1",
+        recommended: "Travel"
+      }
+
+      cards.push(entry);
+
+    });
+    this.cards = cards;
+    console.log("Image from this user : ",JSON.stringify(cards),"\n");
+    });
+  },
+
   mounted(){
     const auth = getAuth();
     const user  = auth.currentUser;
