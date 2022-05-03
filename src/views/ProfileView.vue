2d85cfca207f947eb9ca9a05d81481bef5eccf2a
unn <62011286@kmitl.ac.th>
2022-05-03 22:23:00 +0700

    [UPDATE] Profile page now show images

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -3,21 +3,20 @@
     <TopNav />
     <div class="flex flex-col p-4  items-center justify-center">
         <!-- profile picture -->
-        <div class="w-40 h-40 bg-cover bg-center rounded-full" style="background-image: url(profile.jpg)"></div>
+        <div class="w-40 h-40 bg-cover bg-center rounded-full" style="background-image: url(profile.jpg)" ></div>
         <div class="font-bold">{{DisplayName}}</div>
         <div>{{UserName}}</div>
-        <button class="bg-secondary text-white py-2 px-4 rounded-full">Edit Profile</button>
     </div>
 
-    <div v-for="(card, i) in arr" :key="i" class="w-full">
-      <div class="w-full p-2">
-        <Card :src="`${card.src}`" :board="`${card.recommended}`" class="w-full h-full"/>
-          <div class="text-sm leading-tight pt-2">
-            <p class="font-bold"> {{ card.title }} </p>
-            <p> {{ card.user }} </p>
-          </div>
+    <div class="px-20 w-full flex flex-wrap">
+      <div v-for="(card, index) in cards" :key="index" class="w-1/5">
+        <div class="w-full p-2">
+          <Card :src="`${card.src}`" :board="`${card.recommended}`" class="w-full h-full"/>
+        </div>
       </div>
-    </div>    
+    </div>
+
+
 
     <!-- For filter only user picture -->
     <div class="px-20 w-full flex flex-wrap"></div>
@@ -28,17 +27,20 @@
 <script>
 import TopNav from '../components/TopNav.vue';
 import Card from '../components/CardItem.vue';
-import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { collection, query, where, onSnapshot, addDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import {db} from '@/firebase'
+import {db,storage} from '@/firebase'
+import { ref, uploadString,getDownloadURL  } from "firebase/storage";
 
 export default {
-    components:{ TopNav,Card },
+    components:{ TopNav, Card},
     data(){
         return{
             DisplayName: "",//"Firstname Lastname",
             UserName: "",//"@username",
             cards:[],
+            imageUrl: "",
+            profileImg : getAuth().photoURL,
         }
   },
 
@@ -68,7 +70,14 @@ export default {
     const user  = auth.currentUser;
     this.$data.DisplayName = user.displayName;
     this.$data.UserName = user.email;
-  }
+  },
+
+  methods :{
+    onFilePicked(event){
+
+    },
+  },
+
 }
 </script>
 
